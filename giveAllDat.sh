7935ab59ddeac1e3d24ae75b48d164a83c7907fb
Joel K <91800518+JEnduriumK@users.noreply.github.com>
2022-11-06 00:11:34 -0500

    Update giveAllDat.sh

--- giveAllDat.sh
+++ giveAllDat.sh
@@ -4,7 +4,7 @@
 
 echo "All original .dat files" > alldat.txt
 echo "" >> alldat.txt
-for i in *.dat ; do
+for i in ./input/*.dat ; do 
   [[ -f "$i" ]] || continue
   echo "$i" >> alldat.txt
   echo "------------------------------" >> alldat.txt
@@ -12,3 +12,14 @@ for i in *.dat ; do
   echo "" >> alldat.txt
 done
 echo "All .dat files compiled in to alldat.txt"
+
+echo "All original .asm files" > allasm.txt
+echo "" >> allasm.txt
+for i in ./input/*.asm ; do 
+  [[ -f "$i" ]] || continue
+  echo "$i" >> allasm.txt
+  echo "------------------------------" >> allasm.txt
+  cat "$i" >> allasm.txt
+  echo "" >> allasm.txt
+done
+echo "All .dat files compiled in to allasm.txt"
